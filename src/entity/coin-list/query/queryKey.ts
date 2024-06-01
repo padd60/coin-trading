dfa6986d98ed7255943660a29519c550682b67c2
junghwankim <padd60@naver.com>
2024-06-01 18:11:19 +0900

    fix: entity coin fetch useSuspenseInfiniteQuery 사용으로 변경

--- src/entity/coin-list/query/queryKey.ts
+++ src/entity/coin-list/query/queryKey.ts
@@ -2,9 +2,9 @@ import { createQueryKeys } from '@lukemorales/query-key-factory';
 import { DefaultQueryKeys } from 'src/shared/model';
 
 type CoinListScheme = {
-  page: (page: number, perPage: number) => [number, number];
+  list: null;
 };
 
 export const coinList = createQueryKeys<DefaultQueryKeys, CoinListScheme>('coin-list', {
-  page: (page: number, perPage: number) => [page, perPage],
+  list: null,
 });
--- src/entity/coin-list/query/useGetCoinList.ts
+++ src/entity/coin-list/query/useGetCoinList.ts
@@ -1,17 +1,17 @@
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 import { coinList } from './queryKey';
 import { CoinListRequestParam } from '../model';
 import { getCoinList } from '../api';
 
 export const useGetCoinList = (params: CoinListRequestParam) => {
-  const listParam: CoinListRequestParam = {
-    ...params,
-    page: params.page ? params.page : 1,
-    per_page: params.per_page ? params.per_page : 50,
-  };
+  const limit = params.per_page ?? 50;
 
-  return useSuspenseQuery({
-    queryKey: coinList.page(params.page ?? 1, params.per_page ?? 50).queryKey,
-    queryFn: () => getCoinList(listParam),
+  return useSuspenseInfiniteQuery({
+    queryKey: coinList.list.queryKey,
+    queryFn: ({ pageParam }) => getCoinList({ ...params, page: pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => {
+      return lastPage.length < limit ? undefined : limit;
+    },
   });
 };
