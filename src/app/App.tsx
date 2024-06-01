41d5b9b2822538e96509aeeb2b000259451966e2
junghwankim <padd60@naver.com>
2024-06-02 00:05:27 +0900

    chore: react hot toast 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import router from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,6 +29,15 @@ function App() {
       <NextUIProvider>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
+          <Toaster
+            toastOptions={{
+              style: {
+                backgroundColor: 'black',
+                color: 'white',
+                fontWeight: 'bold',
+              },
+            }}
+          />
           <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
         </QueryClientProvider>
       </NextUIProvider>
