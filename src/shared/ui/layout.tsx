8f944ef4304d86cebbe577012b9018ac0b7cb91c
junghwankim <padd60@naver.com>
2024-06-01 22:59:21 +0900

    design: 레이아웃 내 GNB 스타일 수정

--- src/shared/ui/layout.tsx
+++ src/shared/ui/layout.tsx
@@ -8,9 +8,19 @@ type LayoutProps = {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="h-full w-full p-5">
-      <Tabs fullWidth={true}>
-        <Tab title="가상자산 시세목록">{children}</Tab>
-        <Tab title="북마크 목록">{children}</Tab>
+      <Tabs
+        fullWidth={true}
+        size="lg"
+        classNames={{
+          cursor: '!bg-white',
+        }}
+      >
+        <Tab title="가상자산 시세목록" className="h-fit p-5 text-2xl font-bold">
+          {children}
+        </Tab>
+        <Tab title="북마크 목록" className="h-fit p-5 text-2xl font-bold">
+          {children}
+        </Tab>
       </Tabs>
     </div>
   );
